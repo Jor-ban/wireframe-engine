4113b0c497f8c5401f371435a73e7430b03b5efd
Jor-ban <jam35d12@gmail.com>
2023-07-03 22:19:29 +0500

    implemented decorators (have not tested them all btw)

--- src/engine/examples/extentions/decorators/index.ts
+++ src/engine/examples/extentions/decorators/index.ts
@@ -2,6 +2,13 @@ import { EngineExtensionInterface } from "⚙️/types/EngineExtensionInterface"
 import { EngineInterface } from "⚙️/types/Engine.interface";
 import { CameraJson } from "⚙️/lib/parsers/types/CameraJson.type";
 import { CameraParser } from "⚙️/lib/parsers/cameraParser";
+import { MeshParser } from "⚙️/lib/parsers/MeshParser";
+import { MeshJson } from "⚙️/lib/parsers/types/MeshJson.type";
+import { LightJson } from "⚙️/lib/parsers/types/LightJson.type";
+import { Object3dParser } from "⚙️/lib/parsers/Object3dParser";
+import * as THREE from "three";
+import { Object3dJSON } from "⚙️/lib/parsers/types/Object3DJson.type";
+import { LightParser } from "⚙️/lib/parsers/lightParser";
 
 function logError(decoratorName: string) {
     throw new Error(`[DecoratorsExtension] -> @${decoratorName} -> engine is not defined, \n
@@ -23,19 +30,41 @@ class DecoratorsExtensionFactory implements EngineExtensionInterface {
     get newInstance() {
         return new DecoratorsExtensionFactory()
     }
-    camera(cameraJson: CameraJson) {
+    camera(cameraJson: CameraJson | THREE.Camera) {
         if(!this.engine) logError('Camera')
         return (constructor: Function) => {
             return Object.assign(CameraParser.parse(this.engine.canvasProportion, cameraJson), constructor.prototype)
         }
     }
-    mesh() {
-        if(!this.engine) logError('Mesh')
+    mesh(meshJson: MeshJson | THREE.Mesh) {
         return (constructor: Function) =>  {
-            console.log(constructor);
+            if(meshJson instanceof THREE.Mesh)
+                return Object.assign(meshJson, constructor.prototype)
+
+            return Object.assign(MeshParser.parse(meshJson), constructor.prototype)
         }
+    }
+    light(lightJson: LightJson | THREE.Light) {
+        return (constructor: Function) =>  {
+            return Object.assign(LightParser.parse(lightJson), constructor.prototype)
+        }
+    }
+    object3d(object3dJson: Object3dJSON | THREE.Object3D | undefined) {
+        return (constructor: Function) =>  {
+            if(object3dJson instanceof THREE.Object3D)
+                return Object.assign(object3dJson, constructor.prototype)
+
+            return Object.assign(Object3dParser.setParameters(new THREE.Object3D(), object3dJson), constructor.prototype)
+        }
+    }
+    emptyObject(data: any) {
+        return Object3dParser.setParameters(new THREE.Object3D(), data)
     }
 }
 
 export const DecoratorsExtension = new DecoratorsExtensionFactory()
 export const Camera = DecoratorsExtension.camera.bind(DecoratorsExtension)
+export const Mesh = DecoratorsExtension.mesh
+export const Light = DecoratorsExtension.light
+export const Object3d = DecoratorsExtension.object3d
+export const EmptyObject = DecoratorsExtension.emptyObject
--- src/engine/lib/parsers/types/LoadingMode.enum.ts
+++ src/engine/lib/parsers/types/LoadingMode.enum.ts
@@ -1,4 +1,4 @@
-export enum LoadingMode {
+export enum LoadingMode { // TODO : Cannot implement it now, it needs all the engine to be reworked
     default = 'Default',
     async = 'Async',
 }
